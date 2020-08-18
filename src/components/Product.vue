a7b8d3c0641fc4c1a2e6c1a546b9b2aeb4beabd1
arissatya <arissatya.nugraha@gmail.com>
2020-08-18 20:42:02 +0700

    mencoba vue cli

---
+++ src/components/Product.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="hello">
+    <h1> INI COMPONENT PRODUCTS</h1>
+    <h3> fetch product disini</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Product',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
